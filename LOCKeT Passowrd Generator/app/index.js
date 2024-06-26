11cc4bd4394b9630c11f456dc98503105f1eab6c
Muhammad Hussnain Shaker <mhussnainshaker251@gmail.com>
2024-06-26 14:19:16 +0500

    Developed conditional container height based on number of lines of password for mobile screens. Solved the issue of multiple alters when copying to clipboard.

--- LOCKeT Passowrd Generator/app/index.js
+++ LOCKeT Passowrd Generator/app/index.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
+  
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+  const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const symbols = "!@#$%^&*()-+=_[]{}|;:,./"
+  const numbers = "0123456789"
+  let passwordPicker = ""
+  let finalPassword = ""
+  
   const form = document.querySelector("#form")
-
+  
   form.addEventListener("submit", handleSubmit)
 
   function handleSubmit(event) {
 
-      const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
-      const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-      const symbols = "!@#$%^&*()-+=_[]{}|;:,./"
-      const numbers = "0123456789"
-      let passwordPicker = ""
-      let finalPassword = ""
-      
+    finalPassword = ""
+
     event.preventDefault()
 
     const formData = new FormData(form)
@@ -56,27 +59,57 @@ document.addEventListener("DOMContentLoaded", function () {
     const container = document.querySelector(".result")
     const mainContainer = document.querySelector("#main-container")
 
-    // Code to calculate the number of lines finalPassword is wrapping to
+    // Code to calculate the number of lines finalPassword is wrapping to adjust the height of container accordingly
     const fontSize = getComputedStyle(resultField).fontSize
     const lineHeight = parseFloat(fontSize) * 1.2
     const lines = resultField.clientHeight / lineHeight
     const linesRounded = Math.floor(lines)
 
-    if (linesRounded === 1) {
-      container.style.height = "30px"
-    } else if (linesRounded === 2) {
-      container.style.height = "60px"
-      mainContainer.style.height = "470px"
-    } else if (linesRounded === 3) {
-      container.style.height = "90px"
-      mainContainer.style.height = "500px"
+    const smallScreen = window.matchMedia("(max-width: 480px)")
+
+    if (smallScreen.matches) {
+      if (linesRounded === 1) {
+        container.style.height = "30px"
+        mainContainer.style.height = "570px"
+      } else if (linesRounded === 2) {
+        container.style.height = "60px"
+        mainContainer.style.height = "600px"
+      } else if (linesRounded === 3) {
+        container.style.height = "90px"
+        mainContainer.style.height = "630px"
+      } else if (linesRounded === 4) {
+        container.style.height = "120px"
+        mainContainer.style.height = "660px"
+      } else if (linesRounded >= 5) {
+        container.style.height = "150px"
+        mainContainer.style.height = "690px"
+      }
+    } else {
+      if (linesRounded === 1) {
+        container.style.height = "30px"
+        mainContainer.style.height = "440px"
+      } else if (linesRounded === 2) {
+        container.style.height = "60px"
+        mainContainer.style.height = "470px"
+      } else if (linesRounded === 3) {
+        container.style.height = "90px"
+        mainContainer.style.height = "500px"
+      } else if (linesRounded === 4) {
+        container.style.height = "120px"
+        mainContainer.style.height = "530px"
+      } else if (linesRounded >= 5) {
+        container.style.height = "150px"
+        mainContainer.style.height = "560px"
+      }
     }
 
-    const clipboardIcon = document.querySelector(".clipboard-icon")
-    clipboardIcon.addEventListener("click", () => {
-        navigator.clipboard.writeText(finalPassword)
-        alert("Copied to clipboard")
-    })
-
   }
+
+  const clipboardIcon = document.querySelector(".clipboard-icon")
+  clipboardIcon.addEventListener("click", copyPassword)
+  function copyPassword(event) {
+    navigator.clipboard.writeText(finalPassword)
+    alert("Copied to clipboard")
+  }
+  
 })
